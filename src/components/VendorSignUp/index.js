64b4aedc9eae558ebe40cd9f66f9b6cf3b751e70
Aidan Bane <aidanfbane@gmail.com>
2018-11-14 01:09:21 -0700

    (feat) Initial api connection

--- src/components/VendorSignUp/index.js
+++ src/components/VendorSignUp/index.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import "./VendorSignUp.css";
 
-const VendorSignUp = () => <div className="VendorSignUp">VendorSignUp.js</div>;
+class VendorSignUp extends Component {
+  state = {
+    hello: null
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://rnb57ee82f.execute-api.us-east-1.amazonaws.com/dev/api")
+      .then(({ data: { hello } }) => {
+        this.setState({
+          hello
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { hello } = this.state;
+    return (
+      <div className="VendorSignUp">
+        VendorSignUp.js
+        <h1>Hello, {hello || "..."}</h1>
+      </div>
+    );
+  }
+}
 
 export default VendorSignUp;
